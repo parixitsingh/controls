da27de619d60623b5c56030ca8b85a998718b9e6
unknown <parixitsingh@parixit3790.Nagarro.local>
2018-01-23 02:14:47 +0530

    utility file

---
+++ mewa/mewa.js
@@ -0,0 +1,42 @@
+/*
+Attach methods to window object
+Methods:
+
+	copy(object) : copy method expects a object as a argument
+				   and returns a deep copy of the object.
+*/
+((context)=>{
+   //Deep cloning of a object of primitive or non-primitive type
+   function c(obj){
+   	if(!obj){
+   		return obj;
+   	} else {
+   		let cn = obj.constructor.name;
+   		switch(cn){
+   			case "Boolean":
+   			case "String":
+   			case "Number":
+   			case "Function":
+   				return obj;
+   				break;
+   			case "Date":
+   				return new Date(obj);
+   				break;
+   			case "Array":
+   				let ar = [];
+   				for(let i = 0; i < obj.length; i++){
+   					ar[i] = c(obj[i]);
+   				}
+   				return ar;
+   				break;
+   			case "Object":
+   				let obj1 = {};
+   				Object.keys(obj).forEach((key)=>{
+   					obj1[key] = c(obj[key]);
+   				});
+   				return obj1;
+   		}
+   	}
+   }
+   context.copy = c;
+})(this);
