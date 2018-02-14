c0b75682825e544939ea760d203164a88997f28c
unknown <parixitsingh@parixit3790.Nagarro.local>
2018-02-15 00:59:46 +0530

    made changes for object manipulation examples

--- java/accessModifier/one/Parent.java
+++ java/accessModifier/one/Parent.java
@@ -15,7 +15,7 @@ public class Parent {
 
 
 	public Parent(){
-		System.out.println("I am Parent");
+		//System.out.println("Parent Created");
 	}
 
 	public void PrintName(){
--- java/accessModifier/two/Child.java
+++ java/accessModifier/two/Child.java
@@ -13,13 +13,33 @@ import one.*;
 
 public class Child extends Parent {
 	public Child(){
-		System.out.println("I am Child");
+		//System.out.println("Child Created");
+	}
+
+	public void PrintName(){
+		System.out.println("My name is Child");
 	}
 
 	public static void main(String[] args){
+		//Reference of Child and Object of Child
 		Child c = new Child();
 		c.PrintName();
 
+		//Reference of Parent and Object of Child
+		Parent c1 = new Child();
+		c1.PrintName();
+
+		//Reference of Parent and Object of Parent
+		Parent p = new Parent();
+		p.PrintName();
+
+
+		//Child object assigned to Parent reference
+		p = c;
+
+		//Child object referenced by Parent is type casted into child reference
+		Child c2 = (Child) p;
+		c2.PrintName();
 		// //Trying to access private member of parent
 		// try {
 		// 	System.out.println(c.privateMember);
